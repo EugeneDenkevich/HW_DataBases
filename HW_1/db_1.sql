a415fb201df349ca72bad558f127a190bd47c7b8
EugeneDenkevich <106911093+EugeneDenkevich@users.noreply.github.com>
2022-10-02 12:11:50 +0300

    correct_hw_1

--- HW_1/db_1.sql
+++ HW_1/db_1.sql
@@ -1,20 +1,32 @@
--- You can copy all the code IN the shell AND press ENTER
+-- You can copy all the code in the shell and press ENTER
 -- (if you don't have the base named 'new' of course)
 
 DROP DATABASE IF EXISTS new;
 CREATE DATABASE new; USE new;
 
-CREATE TABLE hamsters (ham_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, the_weight FLOAT NOT NULL, name VARCHAR(20) UNIQUE, coat_color VARCHAR(20), breeds VARCHAR(20), birth DATE);
-INSERT INTO hamsters (the_weight, name, coat_color, breeds, birth) VALUES ('1', '52.7', 'Arnold', 'grey', 'jungarik', '2022-03-14');
-INSERT INTO hamsters (the_weight, name, coat_color, breeds, birth) VALUES ('29.1', 'Chack', 'brown', 'sirian', '2022-06-01');
-INSERT INTO hamsters (the_weight, name, coat_color, breeds, birth) VALUES ('100.8', 'Brus', 'mandarine', 'chinese', '2022-06-17');
-INSERT INTO hamsters (the_weight, name, coat_color, breeds, birth) VALUES ('33.5', 'Stiven', 'white', 'jungarik', '2022-08-06');
-INSERT INTO hamsters (the_weight, name, coat_color, breeds, birth) VALUES ('61.1', 'Silvestr', 'mandarine', 'chinese', '2022-12-31');
-INSERT INTO hamsters (the_weight, name, coat_color, breeds, birth) VALUES ('26.9', 'Mel', 'orange', 'chinese', '2022-12-31');
-INSERT INTO hamsters (the_weight, name, coat_color, breeds, birth) VALUES ('113.54', 'Keanu', 'tan', 'campbell', '2022-12-31');
-INSERT INTO hamsters (the_weight, name, coat_color, breeds, birth) VALUES ('59.2', 'Nikolas', 'cream', 'robkovsy', '2022-12-31');
-INSERT INTO hamsters (the_weight, name, coat_color, breeds, birth) VALUES ('58.7', 'John', 'moss', 'jungarik', '2022-12-31');
-INSERT INTO hamsters (the_weight, name, coat_color, breeds, birth) VALUES ('64.3', 'Will', 'white', 'campbell', '2022-12-31');
+CREATE TABLE hamsters (ham_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(20) UNIQUE,
+breeds VARCHAR(20), birth DATE, the_weight FLOAT NOT NULL, coat_color VARCHAR(20));
+INSERT INTO hamsters (name, breeds, birth, the_weight, coat_color) 
+VALUES ('Arnold', 'jungarik', '2022-03-14', '52.7', 'grey'), 
+('Chack', 'sirian', '2022-06-01', '29.1', 'brown'),
+('Brus', 'chinese', '2022-06-17', '100.8', 'mandarine'),
+('Stiven', 'jungarik', '2022-08-06', '33.5', 'white'),
+('Silvestr', 'chinese', '2022-12-31', '61.1', 'mandarine'),
+('Keanu', 'chinese', '2022-12-31', '26.9', 'orange'),
+('Nikolas', 'campbell', '2022-12-31', '113.54', 'tan'),
+('John', 'robkovsy', '2022-12-31', '59.2', 'cream'),
+('Will', 'jungarik', '2022-12-31', '58.7', 'moss'),
+('Eugene', 'campbell', '2022-12-31', '64.3', 'white');
 
 SELECT  *
 FROM hamsters;
+
+
+
+
+
+
+
+
+ 
+
--- HW_1/db_4.sql
+++ HW_1/db_4.sql
@@ -1,25 +1,22 @@
+-- You can copy all the code in the shell and press ENTER
+-- (if you don't have the base named 'new' of course)
+
 DROP DATABASE IF EXISTS new;
 CREATE DATABASE new;
 USE new;
 
-CREATE TABLE cart (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    manufacturer VARCHAR(30) NOT NULL,
-    availablity ENUM('in stock', 'not in stock'),
-    amount INT NULL DEFAULT 0,
-    UNIQUE (name, manufacturer)
-);
-
-INSERT INTO cart (name, manufacturer, availablity, amount) VALUES ('vacuum cleaner', 'panasonic', 'in stock', 9);
-INSERT INTO cart (name, manufacturer, availablity) VALUES ('TV-324', 'panasonic', 'in stock');
-INSERT INTO cart (name, manufacturer, availablity) VALUES ('vacuum cleaner', 'sony', 'in stock');
-INSERT INTO cart (name, manufacturer, availablity) VALUES ('TV-004', 'sony', 'not in stock');
-INSERT INTO cart (name, manufacturer, availablity) VALUES ('iron-901', 'lg', 'not in stock');
-INSERT INTO cart (name, manufacturer, availablity, amount) VALUES ('iron-113', 'panasonic', 'in stock', 112);
-INSERT INTO cart (name, manufacturer, availablity, amount) VALUES ('TV-001', 'panasonic', 'not in stock', 99);
-INSERT INTO cart (name, manufacturer, availablity, amount) VALUES ('vacuum cleaner', 'sony', 'in stock', 96);
-INSERT INTO cart (name, manufacturer, availablity, amount) VALUES ('TV-654', 'horizont', 'in stock', 13);
-
+CREATE TABLE cart (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30) NOT NULL,
+manufacturer VARCHAR(30) NOT NULL, availablity ENUM('in stock', 'not in stock'),
+amount INT DEFAULT 0, UNIQUE (name, manufacturer));
+INSERT INTO cart (name, manufacturer, availablity)
+VALUES ('TV-324', 'panasonic', 'in stock'),
+('vacuum cleaner', 'sony', 'in stock'),
+('TV-004', 'sony', 'not in stock'),
+('iron-901', 'lg', 'not in stock');
+INSERT INTO cart (name, manufacturer, availablity, amount)
+VALUES ('iron-113', 'panasonic', 'in stock', 112),
+('TV-001', 'panasonic', 'not in stock', 99),
+('iron-900', 'sony', 'in stock', 96),
+('TV-654', 'horizont', 'in stock', 13);
 
 SELECT * FROM cart;
